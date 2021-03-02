017aae0dedd5d9e0e1bc6ff8c8029bf86bb9d335
nimaester <nimaester@gmail.com>
2021-03-01 19:20:57 -0800

    removed pdf file

--- src/NavList.js
+++ src/NavList.js
@@ -4,8 +4,6 @@ import { motion } from "framer-motion";
 
 const NavList = ({refs, setShowLinks, showLinks}) => {
 
-  const pathname = useLocation();
-
   const closeNavLinks = () => {
     setShowLinks(false);
   }
@@ -17,76 +15,34 @@ const NavList = ({refs, setShowLinks, showLinks}) => {
   return (
     <div className={`nav-list ${showLinks ? "active": ""}`}>
       <ul>
-      <li onClick={closeNavLinks} className='primary nav-list-link'>
+        <li onClick={closeNavLinks} className='primary nav-list-link'>
           <NavLink onClick={()=>{scrollSmoothHandler(refs.heroRef)}} to='/' activeClassName='selected' replace>Home</NavLink>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{ width: pathname.pathname === "/" ? "100%" : 0 }}
-            className='current-tab'
-          />
         </li>
+
         <li onClick={closeNavLinks} className='nav-list-link'>
           <NavLink onClick={()=>{scrollSmoothHandler(refs.aboutRef)}} to='/about' activeClassName='selected' replace>About</NavLink>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{
-              width: pathname.pathname === "/about" ? "100%" : 0,
-            }}
-            className='current-tab'
-          />
         </li>
+
         <li onClick={closeNavLinks} className='nav-list-link'>
           <NavLink onClick={()=>{scrollSmoothHandler(refs.skillsRef)}} to='/skills' activeClassName='selected' replace>Skills</NavLink>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{
-              width: pathname.pathname === "/skills" ? "100%" : 0,
-            }}
-            className='current-tab'
-          />
         </li>
+
         <li onClick={closeNavLinks} className='nav-list-link'>
           <NavLink onClick={()=>{scrollSmoothHandler(refs.projectsRef)}} to='/projects' activeClassName='selected' replace>Projects</NavLink>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{
-              width: pathname.pathname === "/projects" ? "100%" : 0,
-            }}
-            className='current-tab'
-          />
         </li>
+
         <li onClick={closeNavLinks} className='nav-list-link'>
           <NavLink onClick={()=>{scrollSmoothHandler(refs.educationRef)}} to='/education' activeClassName='selected' replace>Background</NavLink>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{
-              width: pathname.pathname === "/education" ? "100%" : 0,
-            }}
-            className='current-tab'
-          />
         </li>
+
         <li onClick={closeNavLinks} className='nav-list-link'>
           <NavLink onClick={()=>{scrollSmoothHandler(refs.contactRef)}} to='/contact' activeClassName='selected' replace>Contact</NavLink>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{
-              width: pathname.pathname === "/contact" ? "100%" : 0,
-            }}
-            className='current-tab'
-          />
         </li>
 
         <li onClick={closeNavLinks} className='nav-list-link'>
           <a href='https://drive.google.com/uc?export=download&id=1QmfFCTRXDNGFqpLDS8E7jb1UsJ_tBXOi'>
             Resume
           </a>
-          <motion.div
-            transition={{ duration: 0.75 }}
-            animate={{
-              width: pathname.pathname === "/contact" ? "100%" : 0,
-            }}
-            className='current-tab'
-          />
         </li>
 
       </ul>
